f5c54e80c806fb5ce3d8780e8cb883cddc082bab
Mholl-618 <mholl618@gmail.com>
2017-11-09 16:07:21 -0600

    Final Push
    Fixed equations for upper atmosphere. Cleaned up the input field for /aesthetics/

--- std_atmosphere.m
+++ std_atmosphere.m
@@ -1,4 +1,4 @@
-function [ rho, p,nu, a, t] = std_atmosphere( altitude)
+function [ rho, p,nu, a, t] = std_atmosphere(altitude)
 %UNTITLED Summary of this function goes here
 % Since there is no class today, you will have an extended group lab session.  
 % Each group must have a function and accurate Matlab code that calculates 
@@ -32,14 +32,16 @@ if altitude < 86000 %Troposphere
     return
 % do some stuff here for altitude abvoe 86km
 elseif altitude < 91000 
+    altitude = altitude/1000; %Equations are for z in km
     t = 186.8673;
-    p = exp(2.156582e-06*altitude^3 -4.836957e-04*altitude^2-0.2609971*altitude + 13.47530);
+    p = exp(2.156582e-06*altitude^3 -4.836957e-04*altitude^2-0.1425192*altitude + 13.47530);
     rho = exp(-3.322622e-06*altitude^3 +9.111460e-04*altitude^2-0.1425192*altitude + 5.944694);
     mu = mu_0_earth*((t/earth_t0)^1.5)*((earth_t0+earth_ts)/(t+earth_ts));
 elseif altitude < 100001
+    altitude = altitude/1000; %Equations are for z in km
     delta_z = altitude - 91000;
     little_a = -19942.9;
-    t = 263.1905-76*sqrt(1-((delta_z/little_a)^2));
+    t = 263.1905-76.3232*sqrt(1-((delta_z/little_a)^2));
     p = exp(3.304895e-05*altitude^3 -0.009062730e-04*altitude^2+0.6516698*altitude - 11.03037);
     rho = exp(2.873405e-05*altitude^3 - 0.008492037*altitude^2+0.6541179*altitude -23.62010);
     mu = mu_0_earth*((t/earth_t0)^1.5)*((earth_t0+earth_ts)/(t+earth_ts));
